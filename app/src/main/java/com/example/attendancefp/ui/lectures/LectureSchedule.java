91d18c8bb647696a2311cfa30ba03295e1ef9a2a
MustafaL8 <mustafa.ayerdem@l8studios.com>
2022-12-25 04:49:18 +0300

    list view is added to lecture schedule

--- app/src/main/java/com/example/attendancefp/ui/lectures/LectureSchedule.java
+++ app/src/main/java/com/example/attendancefp/ui/lectures/LectureSchedule.java
@@ -1,18 +1,34 @@
 package com.example.attendancefp.ui.lectures;
 
+import android.app.ListActivity;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.attendancefp.R;
 
+import java.util.ArrayList;
+import java.util.Objects;
 
-public class LectureSchedule extends Fragment {
 
+public class LectureSchedule extends Fragment implements AdapterView.OnItemClickListener {
+
+    ArrayList<String> Lectures = new ArrayList<String>(); ;
+    Button btn;
+    ListView listView ;
+    ArrayAdapter<String> adapter;
     public LectureSchedule() {
         // Required empty public constructor
     }
@@ -25,5 +41,40 @@ public class LectureSchedule extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_lecture_schedule, container, false);
+
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        btn = (Button) requireView().findViewById(R.id.btton);
+
+        Lectures.add("web");
+        listView =(ListView)view.findViewById(R.id.lst);
+        adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1,Lectures);
+
+
+
+        btn.setOnClickListener(this::onClick);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        if(i==0){
+            Toast.makeText(getActivity(),"NLP", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+
+    public void onClick(View v) {
+        Lectures.add("NLP"+"                   "+"Monday 12:00 - 14:50");
+
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
+
+    }
+
+
+
 }
