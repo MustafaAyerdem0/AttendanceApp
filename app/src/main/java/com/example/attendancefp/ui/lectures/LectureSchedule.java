025223b1dff0bb50f0bc16cd7be0e89b187c36c8
MustafaL8 <mustafa.ayerdem@l8studios.com>
2022-12-26 02:16:04 +0300

    Lecture Schedule is updated from firebase

--- app/src/main/java/com/example/attendancefp/ui/lectures/LectureSchedule.java
+++ app/src/main/java/com/example/attendancefp/ui/lectures/LectureSchedule.java
@@ -38,7 +38,7 @@ public class LectureSchedule extends Fragment implements AdapterView.OnItemClick
     ArrayList<String> Lectures = new ArrayList<String>(); ;
     Button btn;
     ListView listView ;
-    ArrayAdapter<String> adapter;
+    ArrayAdapter adapter;
     public LectureSchedule() {
         // Required empty public constructor
     }
@@ -47,29 +47,39 @@ public class LectureSchedule extends Fragment implements AdapterView.OnItemClick
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        ListSchedule();
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_lecture_schedule, container, false);
+
+
 
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        btn = (Button) requireView().findViewById(R.id.btton);
+        //btn = (Button) requireView().findViewById(R.id.btton);
 
-        Lectures.add("web");
+        //Lectures.add("web");
+
+
+        //btn.setOnClickListener(this::onClick);
+
+        //ListSchedule();
+
         listView =(ListView)view.findViewById(R.id.lst);
-        adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1,Lectures);
+        ListSchedule();
 
 
 
-        btn.setOnClickListener(this::onClick);
+
     }
 
     @Override
@@ -80,17 +90,21 @@ public class LectureSchedule extends Fragment implements AdapterView.OnItemClick
 
     }
 
-
+/*
     public void onClick(View v) {
-        Lectures.add("NLP"+"                   "+"Monday 12:00 - 14:50");
-
+        //Lectures.add("NLP"+"                   "+"Monday 12:00 - 14:50");
+        adapter = new ArrayAdapter<String>(getContext(), R.layout.lectures_schedule_text_setting_layout,Lectures);
+        Lectures.add("sdkfsdf");
         listView.setAdapter(adapter);
-        listView.setOnItemClickListener(this);
+        //listView.setOnItemClickListener(this);
 
     }
-
+*/
     public void ListSchedule()
     {
+
+
+
         mUser = mAuth.getCurrentUser(); //giriş yapan kullanıcının bilgilerini alıyoruz bu işlemle
 
         //mReference = FirebaseDatabase.getInstance().getReference(); // firebase deki root bölümü alır .
@@ -102,8 +116,14 @@ public class LectureSchedule extends Fragment implements AdapterView.OnItemClick
                 for(DataSnapshot snp: snapshot.getChildren())
                 {
                     System.out.println(snp.getValue());
-                    System.out.println(snp.getKey()+ " =" +snp.getValue());
+                    System.out.println(snp.getKey()+ " = " +snp.getValue());
+                    Lectures.add(snp.getKey()+ "\n\n" +snp.getValue());
+                    System.out.println(Lectures+"----------*****************-------------------");
                 }
+
+                adapter = new ArrayAdapter<String>(getContext(), R.layout.lectures_schedule_text_setting_layout,Lectures);
+                listView.setAdapter(adapter);
+                //listView.setOnItemClickListener(this);
 
             }
 
@@ -113,6 +133,10 @@ public class LectureSchedule extends Fragment implements AdapterView.OnItemClick
                 Toast.makeText(getActivity(), error.getMessage(), Toast.LENGTH_SHORT).show(); //içinde bulunudğumuz fragmnet a this ile değilde getactivity ile ulaşabiliyoruz
             }
         });
+
+
+
+
     }
 
 
