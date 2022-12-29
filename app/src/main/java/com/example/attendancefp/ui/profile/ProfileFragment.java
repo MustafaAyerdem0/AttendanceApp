c438fcb9198acb056ef41f5d96ca6e4e87b22cfa
MustafaL8 <mustafa.ayerdem@l8studios.com>
2022-12-30 00:01:23 +0300

    Profile Fragment transition completed

---
+++ app/src/main/java/com/example/attendancefp/ui/profile/ProfileFragment.java
@@ -0,0 +1,46 @@
+package com.example.attendancefp.ui.profile;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.attendancefp.R;
+import com.example.attendancefp.databinding.FragmentAboutBinding;
+import com.example.attendancefp.databinding.FragmentProfileBinding;
+import com.example.attendancefp.ui.about.AboutViewModel;
+
+public class ProfileFragment extends Fragment {
+
+    private FragmentProfileBinding binding;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        ProfileViewModel profileViewModel =
+                new ViewModelProvider(this).get(ProfileViewModel.class);
+
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textAbout;
+        profileViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+
+}
